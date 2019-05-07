444566bbfea95802352ba23f521899d3f089b775
Flavio <flavioluismilani@hotmail.com>
2019-05-07 15:38:15 -0300

    Exercício Modificação

--- teste.cpp
+++ teste.cpp
@@ -4,9 +4,14 @@ using namespace std;
 
 int main(){
 
+    string x;
+
     cout << "Teste" << endl;
     cout << "Teste 2" << endl;
 
-    return 0;
+    cout << "Teste 3, Digite algo: ";
+    cin >> x;
+
+    cout << "Voce digitou: " << x << endl;
 
 }
